0b64c9c4656c5b7a01cc07413a73e88935cd1f47
hvngover <hvngover17@gmail.com>
2023-04-19 19:25:07 +0600

    arraylist

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,3 +1,26 @@
 public class MyArrayList<E> implements MyList<E> {
+    private static final int INITIAL_CAPACITY = 10;
+    private E[] data = (E[]) new Object[INITIAL_CAPACITY];
+    private int size = 0;
 
+    public void add(E e) {
+        if (size == data.length) {
+            resize();
+        }
+        data[size++] = e;
+    }
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (size == data.length) {
+            resize();
+        }
+        for (int i = size; i > index; i--) {
+            data[i] = data[i - 1];
+        }
+        data[index] = e;
+        size++;
+    }
 }
