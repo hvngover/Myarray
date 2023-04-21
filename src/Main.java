011260a6330b681b432b293ada33d38d0d3d7dc8
hvngover <hvngover17@gmail.com>
2023-04-21 20:46:46 +0600

    main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         MyList<Integer> list = new MyArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list.get(1));
+        list.remove(1);
+        System.out.println(list.get(1));
 
         list = new MyLinkedList<>();
-
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list.get(1));
+        list.remove(1);
+        System.out.println(list.get(1));
     }
-
 }
