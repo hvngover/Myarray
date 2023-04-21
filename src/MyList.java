b39dfe09b2c20ff3647f39dffe1a5dbdb4eee1f0
hvngover <hvngover17@gmail.com>
2023-04-21 20:51:06 +0600

    mylist

--- src/MyList.java
+++ src/MyList.java
@@ -1,7 +1,12 @@
-public interface MyList<E> {
+public interface MyList<E> { // This is the interface for a simple list data structure
+    // Adds an element to the end of the list
     void add(E e);
+    // Adds an element at the specified index in the list
     void add(int index, E e);
+    // Retrieves the element at the specified index in the list
     E get(int index);
+    // Removes the element at the specified index in the list and returns it
     E remove(int index);
+    // Returns the number of elements in the list
     int size();
 }
