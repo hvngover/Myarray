86887004856ff81d77a5933d860b5b49be00632b
hvngover <hvngover17@gmail.com>
2023-04-21 12:13:41 +0600

    link

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,7 +1,10 @@
 public class MyLinkedList<E> implements MyList<E> {
-    private Node<E> head;
-    private int size;
+    private Node<E> head; // reference to the first node in the list
+    private int size; // number of elements in the list
 
+    // adds the specified element to the end of the list
     public void add(E e) {
         add(size, e);
     }
+
+}
