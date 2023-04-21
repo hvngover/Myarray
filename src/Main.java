b874c2835d96cb24f725e54e8a2a3560bd78596f
hvngover <hvngover17@gmail.com>
2023-04-21 20:54:51 +0600

    mylist

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-public class Main {
+public class Main { // This is the main class that tests the MyArrayList and MyLinkedList implementations of MyList interface
     public static void main(String[] args) {
+        // Testing MyArrayList
         MyList<Integer> list = new MyArrayList<>();
         list.add(1);
         list.add(2);
@@ -8,6 +9,7 @@ public class Main {
         list.remove(1);
         System.out.println(list.get(1));
 
+        // Testing MyLinkedList
         list = new MyLinkedList<>();
         list.add(1);
         list.add(2);
