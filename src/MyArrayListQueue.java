38bdeac703a1d9a205209364a820f7847a54adc4
hvngover <hvngover17@gmail.com>
2023-05-01 22:39:47 +0600

    queue

---
+++ src/MyArrayListQueue.java
@@ -0,0 +1,13 @@
+import java.util.NoSuchElementException;
+
+public class MyArrayListQueue<E> implements MyQueue<E> {
+    private MyArrayList<E> queue;
+
+    public MyArrayListQueue() {
+        queue = new MyArrayList<>();
+    }
+
+
+
+
+}
