e3c793988cfa30b264aadb309103536628f499b7
hvngover <hvngover17@gmail.com>
2023-05-01 22:41:07 +0600

    queue

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -3,6 +3,7 @@ import java.util.NoSuchElementException;
 public class MyArrayListQueue<E> implements MyQueue<E> {
     private MyArrayList<E> queue;
 
+    // Constructor initializes the queue with an empty MyArrayList
     public MyArrayListQueue() {
         queue = new MyArrayList<>();
     }
