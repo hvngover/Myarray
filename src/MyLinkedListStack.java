58d318c3f1cef97242effbd98156bd65bdbf7574
hvngover <hvngover17@gmail.com>
2023-05-01 21:51:42 +0600

    stack

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,2 +1,11 @@
-public class MyLinkedListStack {
+import java.util.LinkedList;
+import java.util.EmptyStackException;
+
+public class MyLinkedListStack<E> implements MyStack<E> {
+    private LinkedList<E> stack;
+
+    public MyLinkedListStack() {
+        stack = new LinkedList<>();
+    }
+
 }
