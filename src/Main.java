8a9ed779d03e8f166d65c451325f93b439c5a650
hvngover <hvngover17@gmail.com>
2023-05-01 23:18:25 +0600

    queue

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,23 @@
 public class Main { // This is the main class that tests the MyArrayList and MyLinkedList implementations of MyList interface
     public static void main(String[] args) {
-        // Testing MyArrayList
-        MyList<Integer> list1 = new MyArrayList<>();
-        list1.add(1);
-        list1.add(2);
-        list1.add(3);
-        list1.add(2);
-        list1.add(4);
-        System.out.println("Before removing duplicates: " + list1);
-        list1.delete_duplicates();
-        System.out.println("After removing duplicates: " + list1);
+        // Testing MyLinkedListStack
+        MyStack<Integer> stack1 = new MyLinkedListStack<>();
+        stack1.push(1);
+        stack1.push(2);
+        stack1.push(3);
+        System.out.println("Stack size: " + stack1.size());
+        System.out.println("Top element: " + stack1.peek());
+        System.out.println("Pop element: " + stack1.pop());
+        System.out.println("Top element: " + stack1.peek());
 
-        MyList<String> list2 = new MyLinkedList<>();
-        list2.add("a");
-        list2.add("b");
-        list2.add("c");
-        list2.add("b");
-        list2.add("d");
-        System.out.println("Before removing duplicates: " + list2);
-        list2.delete_duplicates();
-        System.out.println("After removing duplicates: " + list2);
-
+        // Testing MyArrayListQueue
+        MyQueue<String> queue1 = new MyArrayListQueue<>();
+        queue1.enqueue("a");
+        queue1.enqueue("b");
+        queue1.enqueue("c");
+        System.out.println("Queue size: " + queue1.size());
+        System.out.println("Front element: " + queue1.peek());
+        System.out.println("Dequeue element: " + queue1.dequeue());
+        System.out.println("Front element: " + queue1.peek());
     }
 }
