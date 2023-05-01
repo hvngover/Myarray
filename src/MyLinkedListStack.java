37c7f5b224ce0c3cee219cde6885d533e49712ff
hvngover <hvngover17@gmail.com>
2023-05-01 21:53:53 +0600

    stack

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -4,6 +4,7 @@ import java.util.EmptyStackException;
 public class MyLinkedListStack<E> implements MyStack<E> {
     private LinkedList<E> stack;
 
+    // Constructor initializes an empty linked list
     public MyLinkedListStack() {
         stack = new LinkedList<>();
     }
